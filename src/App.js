e14a8826b96e63a1468a4910edc74935b99e0f97
Clement Vachet <cvachet@sci.utah.edu>
2017-09-15 11:58:24 -0600

    Improve Higlass UI with bootstrap and include several ViewConfig

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
 import './App.css';
-import { createHgComponent } from 'higlass';
-import {testViewConfig} from './config'
-import './Components/HiGlassLauncher.scss';
+import * as hglib from 'higlass';
+import * as ViewConfig from './ViewConfig'
+//import './Components/HiGlassLauncher.scss';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
   render() {
+    var testViewConfig = ViewConfig.ViewConfig_Default;
+    //window.higlassApi;
     return (
       <div className="App">
         <div className="App-header">
           <h2>Copy Number Project</h2>
         </div>
+        <div>
 
-          {createHgComponent(
-          document.getElementById('higlass'),
-          testViewConfig,
-          { bounded: true },
-          function (api) {
+          {hglib.createHgComponent(
+            document.getElementById('higlass'),
+            testViewConfig,
+            { bounded: true },
+            function (api) {
               window.hgApi = api;
-          }
-      )}        
+            }
+          )}
+        </div>
       </div>
     );
   }
