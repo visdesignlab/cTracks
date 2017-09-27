f170899454cfdaa14416d5decb6510267ddbf970
Clement Vachet <cvachet@sci.utah.edu>
2017-09-26 18:49:08 -0600

    Enable processing with CNV: file upload, data parsing and data filtering

--- src/App.js
+++ src/App.js
@@ -3,20 +3,43 @@ import * as ViewConfig from './ViewConfig';
 import HiglassUI from './HiglassUI';
 import CNVTable from './CNVTable';
 import HiglassAPI from './utils/HiglassAPI';
+import ReactFileReader from 'react-file-reader';
+import Papa from 'papaparse';
 
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 //import './Components/HiGlassLauncher.scss';
 
+function ParseFile(file, callback) {
+  console.log("Parsing CNV file...");
+
+  Papa.parse(file, {
+    //download: true,
+    newline: "\n",
+    header: true,
+    dynamicTyping: true,
+    error: function(error) {
+      console.log("ERROR Parse:",error);
+    },
+    complete: function(results) {
+      callback(results.data);
+    }
+  });
+}
 
 class App extends Component {
   constructor (props) {
     super(props);
     this.state = {
       APIInfo: null,
+      CNVData: null,
     };
     this.UpdateLocation = this.UpdateLocation.bind(this);
+    this.UpdateLocation2 = this.UpdateLocation2.bind(this);
+    this.UpdateAPIInfo = this.UpdateAPIInfo.bind(this);
+    this.ProcessCNVFile = this.ProcessCNVFile.bind(this);
+    this.UpdateCNVData = this.UpdateCNVData.bind(this);
   }
 
   componentDidMount() {
@@ -34,7 +57,7 @@ class App extends Component {
   }
 
   UpdateLocation() {
-    console.log("UpdateAPIInfo...");
+    console.log("Updating location...");
     //HiglassAPI.fetchLocation('aa')
     HiglassAPI.fetchLocationAuto()
       .then(function(location) {
@@ -42,27 +65,50 @@ class App extends Component {
       }.bind(this));
   }
 
+  ListenerID (id) {
+    console.log('Listener ID:', id);
+  } 
+
   UpdateLocation2() {
-    console.log("UpdateAPIInfo2...");
-    var ViewID = HiglassAPI.fetchViewConfig()
-      .then((ViewID) => {return ViewID});
-    console.log("ViewID:", ViewID);
-/*
-    window.hgApi.on('location', (info) => {
-      this.setState({APIInfo: info});
-      console.log('We are over here:', info);
-    }, ViewID , function (id) {
-      console.log('Second Listener ID:', id);
-      });
+    console.log("Updating location2...");
+    /*HiglassAPI.fetchViewConfig()
+      .then(function(ViewID) {
+        window.hgApi.on('location',(location) => {this.UpdateAPIInfo(location)},ViewID, function (id) {
+          console.log('Second Listener ID:', id);
+          });
+        });
 */
+//    window.hgApi.on('location', this.UpdateAPIInfo, 'aa' , this.ListenerID);
 
     // Need to improve function (with automated viewID)
-    window.hgApi.on('location', (info) => {
-      this.setState({APIInfo: info});
+   window.hgApi.on('location', (info) => {
       console.log('We are over here:', info);
+      //this.setState({APIInfo: info});
     }, 'aa' , function (id) {
-      console.log('Second Listener ID:', id);
+      console.log('Listener ID:', id);
       });
+  }
+
+  ProcessCNVFile (files) {
+    console.log("Processing CNVFile...");
+    // Loading file
+    var reader = new FileReader();
+    reader.onload = function(e) {
+      // Use reader.result
+      var text = reader.result;
+    }
+    //reader.readAsText(files[0]);
+
+    ParseFile(files[0], this.UpdateCNVData);  
+  }
+
+  UpdateCNVData(data) {
+    console.log("CNVData:", data);
+    this.setState(function () {
+      return {
+        CNVData: data
+      }
+    });
   }
 
   render() {
@@ -73,11 +119,16 @@ class App extends Component {
         <div>
           <HiglassUI ViewConfig = {MyViewConfig} />
         </div>
+        <div  className = "Button">
+          <ReactFileReader handleFiles={this.ProcessCNVFile} fileTypes={'.bed, .tsv'}>
+            <button>Upload CNV File</button>
+          </ReactFileReader>
+        </div>
         <div className = "Button">
           <button onClick={this.UpdateLocation}>UpdateTable</button>
         </div>
         <div>
-          {this.state.APIInfo && <CNVTable location={this.state.APIInfo} />}
+          {this.state.APIInfo && this.state.CNVData && <CNVTable CNVData = {this.state.CNVData} location={this.state.APIInfo} />}
         </div>
       </div>
     )
--- src/CNVTable.js
+++ src/CNVTable.js
@@ -7,10 +7,9 @@ import JsonTable from 'react-json-table';
 import './CNVTable.css';
 
 
-
-// Retrieve CNV information 
-function RetrieveTable (location) {
-	console.log("Retrieve table from file...");
+// WARNING: OLD function - Retrieve CNV information 
+/*function RetrieveTable (location) {
+	console.log("Retrieving table from file...");
 
 
  // Retrieve information from csvString
@@ -39,27 +38,83 @@ function RetrieveTable (location) {
 
 
   // Retrieve information from url
-/*  var CSVFile_URL = "http://sci.utah.edu/~cvachet/Documents/BIDAC/Project_ARUP/data/test_data.tsv";
-  var CSVFile_DataBase = "http://localhost:8989/api/v1/tiles/?d=test_data";
-  var CSVFile_DataBase2 = "http://localhost:8000/test_data.tsv";
-  var CSVFile_DataBase3 = "http://scis-macbook-pro.local:3000/src/data/test_data.tsv";
-  var CSVFile_DataBase4 = "/data/test_data.tsv";
+  // var CSVFile_URL = "http://sci.utah.edu/~cvachet/Documents/BIDAC/Project_ARUP/data/test_data.tsv";
+  // var CSVFile_DataBase = "http://localhost:8989/api/v1/tiles/?d=test_data";
+  // var CSVFile_DataBase2 = "http://localhost:8000/test_data.tsv";
+  // var CSVFile_DataBase3 = "http://scis-macbook-pro.local:3000/src/data/test_data.tsv";
+  // var CSVFile_DataBase4 = "/data/test_data.tsv";
 
-  Papa.parse(CSVFile_DataBase3, {
-    download: true,
-    error: function(error) {
-      console.log("ERROR Parse:",error);
-    },
-    complete: function(results) {
-      console.log("CSVInfo_DataBase:",results);
-    }
-  });
-*/
+  // Papa.parse(CSVFile_DataBase3, {
+  //   download: true,
+  //   error: function(error) {
+  //     console.log("ERROR Parse:",error);
+  //   },
+  //   complete: function(results) {
+  //     console.log("CSVInfo_DataBase:",results);
+  //   }
+  // });
+
 
 	// Filter data with location information
 	return results.data;
 }
+*/
 
+// Filter CVN data using location information (from ViewConfig) 
+function FilterInfo(location,data) {
+    console.log("Filtering CNV information...");
+    // Temporary information: viewConfig Box
+    //var location = [4, 12859180, 19, 59080857];
+    var Keys = Object.keys(data[0]);
+
+    var output = [];
+    var searchField1 = Keys[0]; // "chrom"
+    var searchField2 = Keys[1]; // "start"
+    var searchField3 = Keys[2]; // "stop"
+    var searchVal1 = location[0].replace('chr','');
+    var searchVal2 = location[1];
+    var searchVal3 = location[2].replace('chr','');
+    var searchVal4 = location[3];
+
+    
+    // Multiple rounds for search: 
+    var Index_Lower = 0;
+    var Index_Upper = data.length;
+      // Step 1: find lower index 
+    for (let i = 0 ; i < data.length ; i++)
+    {
+        if (data[i][searchField1] < searchVal1) {
+          continue;
+        } else if ((data[i][searchField1] > searchVal1) | ((data[i][searchField1] == searchVal1) & (data[i][searchField2] >= searchVal2)))
+        {
+          Index_Lower = i;
+          break;
+        }
+    }
+
+      // Step 2: find upper index 
+    for (let i = Index_Lower; i < data.length ; i++)
+    {
+        if (data[i][searchField1] < searchVal3) {
+          continue;
+        } if ((data[i][searchField1] > searchVal3) | ((data[i][searchField1] == searchVal3) & (data[i][searchField3] > searchVal4)))
+        {
+          //console.log("In Upper loop...");
+          Index_Upper = i;
+          break;
+        }
+    }
+
+    console.log("Data_Index_Lower:", Index_Lower);
+    console.log("Data_Index_Upper:", Index_Upper);
+
+    // Step3: return filtered table
+    for (let i = Index_Lower; i < Index_Upper; i++)
+    {
+      output.push(data[i]);
+    }
+    return output;
+}
 
 class CNVTable extends Component {
   constructor (props) {
@@ -68,20 +123,48 @@ class CNVTable extends Component {
       Table: null,
     };
 
-    this.UpdateTable = this.UpdateTable.bind(this);
+    //this.UpdateTable = this.UpdateTable.bind(this);
   }
 
   componentDidMount() {
-    this.UpdateTable();
+    this.FilterCNVInfo();
   }
 
-  UpdateTable() {
-    var CNVData = RetrieveTable(this.props.location);
+  componentWillReceiveProps(nextProps) {
+    console.log("In componentWillReceiveProps");
+    var ThisLocationString = this.props.location.toString();
+    var NextLocationString = nextProps.location.toString();
+    var ThisCNVDataLength = this.props.CNVData.length;
+    var NextCNVDataLength = nextProps.CNVData.length;
+    console.log('ThisCNVDataLength', ThisCNVDataLength);
+    console.log('NextCNVDataLength', NextCNVDataLength);
+    if ((ThisLocationString !== NextLocationString) | (ThisCNVDataLength !== NextCNVDataLength)) {
+      this.FilterCNVInfo2(nextProps);
+    }
+  }
+
+  // Process 
+  FilterCNVInfo() {
+    // Old function to retrieve info from csvString
+    //var CNVData = RetrieveTable(this.props.location);
+    var TableInfo = FilterInfo(this.props.location,this.props.CNVData);
+    console.log('TableInfo (post-filtering)',TableInfo);
+    this.UpdateTable(TableInfo);
+  }
+
+  FilterCNVInfo2(nextProps) {
+    var TableInfo = FilterInfo(nextProps.location,nextProps.CNVData);
+    console.log('TableInfo (post-filtering)',TableInfo);
+    this.UpdateTable(TableInfo);
+  }
+
+  // Update state of Table
+  UpdateTable (TableInfo) {
     this.setState(function () {
-          return {
-            Table: CNVData
-          }
-        });
+      return {
+        Table: TableInfo
+      }
+    });
   }
 
 	render () {
