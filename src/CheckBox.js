9d3c4ead48aa046574853b36a64f071d3f25db64
Clement Vachet <cvachet@sci.utah.edu>
2017-11-06 16:26:09 -0700

    Enable Tracks Menu

---
+++ src/CheckBox.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class CheckBox extends Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      isChecked: this.props.InitialState
+    }
+  }
+
+  toggleCheckBoxChange = () => {
+    const { handleCheckBoxChange, label } = this.props;
+
+    this.setState(({ isChecked }) => (
+      {
+        isChecked: !isChecked,
+      }
+    ));
+
+    handleCheckBoxChange(label);
+  }
+
+  render() {
+    const { label } = this.props;
+    const { isChecked } = this.state;
+
+    return (
+      <div className="checkBox">
+        <label>
+          <input
+            type="checkBox"
+            value={label}
+            checked={isChecked}
+            onChange={this.toggleCheckBoxChange}
+          />
+
+          {label}
+        </label>
+      </div>
+    );
+  }
+}
+
+CheckBox.propTypes = {
+  label: PropTypes.string.isRequired,
+  handleCheckBoxChange: PropTypes.func.isRequired,
+};
+
+export default CheckBox;
